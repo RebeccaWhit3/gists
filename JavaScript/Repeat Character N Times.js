90420b883806cecadad84c95583342861449f453
RebeccaWhit3 <rebecca.whit3@gmail.com>
2017-05-02 07:22:41 -0400

    Create Repeat Character N Times.js

---
+++ JavaScript/Repeat Character N Times.js
@@ -0,0 +1,7 @@
+// Repeat Character N Times
+Array(10+1).join("a") // create string with 10 as "aaaaaaaaaa"
+
+// Notes:
+// • Array.join inserts content between array elements. i.e An Array.Join of length 11 will result in a 10 character string
+// • For loops are faster in Safari and Chrome (but not Firefox)
+// • Adapted from: stackoverflow.com/q/1877475
